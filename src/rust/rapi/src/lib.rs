e84fcb294ffaa6218c2221bc4537d9070be54000
cgmossa <cgmossa@gmail.com>
2023-04-16 21:46:12 +0200

    move r-api

--- src/rust/rapi/src/lib.rs
+++ src/rust/rapi/src/lib.rs
@@ -12,3 +12,20 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+use extendr_api::prelude::*;
+
+/// Return string `"Hello world!"` to R.
+/// @export
+#[extendr]
+fn hello_world() -> &'static str {
+    "Hello world!"
+}
+
+// Macro to generate exports.
+// This ensures exported functions are registered with R.
+// See corresponding C code in `entrypoint.c`.
+extendr_module! {
+    mod rdoctool;
+    fn hello_world;
+}
